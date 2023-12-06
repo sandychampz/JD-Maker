9428d7ca3752812d0878d65aba4d8e325730d223
sandeep <sandeep@PF3DHWHQ-LENOVO>
2023-12-06 12:30:54 +0530

    Add project files.

---
+++ JD Maker/Form1.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace JD_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.sample = new System.Windows.Forms.RichTextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.label13 = new System.Windows.Forms.Label();
+            this.XX2 = new System.Windows.Forms.TextBox();
+            this.XX1 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.bill = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.metadesc = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.region = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.suburb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.profession1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 803);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 114);
+            this.button1.TabIndex = 61;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sample
+            // 
+            this.sample.Location = new System.Drawing.Point(841, -21);
+            this.sample.Name = "sample";
+            this.sample.Size = new System.Drawing.Size(957, 945);
+            this.sample.TabIndex = 60;
+            this.sample.Text = resources.GetString("sample.Text");
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter1.Location = new System.Drawing.Point(823, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(999, 1238);
+            this.splitter1.TabIndex = 59;
+            this.splitter1.TabStop = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(597, 104);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(14, 20);
+            this.label13.TabIndex = 58;
+            this.label13.Text = "-";
+            // 
+            // XX2
+            // 
+            this.XX2.Location = new System.Drawing.Point(615, 101);
+            this.XX2.Name = "XX2";
+            this.XX2.Size = new System.Drawing.Size(47, 26);
+            this.XX2.TabIndex = 57;
+            // 
+            // XX1
+            // 
+            this.XX1.Location = new System.Drawing.Point(544, 101);
+            this.XX1.Name = "XX1";
+            this.XX1.Size = new System.Drawing.Size(47, 26);
+            this.XX1.TabIndex = 56;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(449, 104);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 20);
+            this.label12.TabIndex = 55;
+            this.label12.Text = "Billing ratio:";
+            // 
+            // bill
+            // 
+            this.bill.FormattingEnabled = true;
+            this.bill.Items.AddRange(new object[] {
+            "Mixed Billings",
+            "Private Billings",
+            "Other Billings"});
+            this.bill.Location = new System.Drawing.Point(543, 54);
+            this.bill.Name = "bill";
+            this.bill.Size = new System.Drawing.Size(178, 28);
+            this.bill.TabIndex = 54;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(449, 57);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(88, 20);
+            this.label11.TabIndex = 53;
+            this.label11.Text = "Billing type:";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(243, 787);
+            this.richTextBox4.MaxLength = 1000;
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(366, 201);
+            this.richTextBox4.TabIndex = 52;
+            this.richTextBox4.Text = "Permanent Position \nFull time or Part time Engagement type\nMMM 1 location\n65% - 7" +
+    "0% of Mixed Billings\nImmediate commencement\nRemuneration to be considered ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(106, 803);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(131, 20);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "Profile Highlights:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(243, 651);
+            this.richTextBox3.MaxLength = 500;
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(658, 112);
+            this.richTextBox3.TabIndex = 50;
+            this.richTextBox3.Text = "The Medical Practice is conveniently located in the heart of 111, Australia and i" +
+    "t treats all locals and surrounding communities with high quality care. ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(138, 675);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 20);
+            this.label9.TabIndex = 49;
+            this.label9.Text = "Emp Details:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(243, 513);
+            this.richTextBox2.MaxLength = 1000;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(658, 119);
+            this.richTextBox2.TabIndex = 48;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 532);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 20);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "Job Brief:";
+            // 
+            // metadesc
+            // 
+            this.metadesc.Location = new System.Drawing.Point(243, 413);
+            this.metadesc.MaxLength = 160;
+            this.metadesc.Name = "metadesc";
+            this.metadesc.Size = new System.Drawing.Size(658, 52);
+            this.metadesc.TabIndex = 46;
+            this.metadesc.Text = "Explore General Practitioner opportunities in 111, Australia. Apply now for the b" +
+    "est GP jobs with competitive salaries and benefits at Medfuture, Australia.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 429);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(192, 20);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "Custom Meta Description:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Medical Doctors",
+            "Nurses",
+            "Allied Health Professionals",
+            "Dentists",
+            "Healthcare Executives and Managers",
+            "Psychologists"});
+            this.comboBox3.Location = new System.Drawing.Point(245, 358);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(253, 28);
+            this.comboBox3.TabIndex = 44;
+            // 
+            // region
+            // 
+            this.region.Location = new System.Drawing.Point(245, 306);
+            this.region.Name = "region";
+            this.region.Size = new System.Drawing.Size(253, 26);
+            this.region.TabIndex = 43;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(131, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 20);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "Vacancy Info:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Region:";
+            // 
+            // suburb
+            // 
+            this.suburb.Location = new System.Drawing.Point(245, 256);
+            this.suburb.Name = "suburb";
+            this.suburb.Size = new System.Drawing.Size(253, 26);
+            this.suburb.TabIndex = 40;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Suburb:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "NSW",
+            "QLD",
+            "VIC",
+            "SA",
+            "TAS",
+            "WA",
+            "NT"});
+            this.comboBox2.Location = new System.Drawing.Point(245, 203);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(253, 28);
+            this.comboBox2.TabIndex = 38;
+            this.comboBox2.Text = "NSW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "State:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(258, 87);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(127, 24);
+            this.radioButton2.TabIndex = 36;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "New Zealand";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(113, 86);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 24);
+            this.radioButton1.TabIndex = 35;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Australia";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Country:";
+            // 
+            // profession1
+            // 
+            this.profession1.FormattingEnabled = true;
+            this.profession1.Items.AddRange(new object[] {
+            "General Practitioner",
+            "Psychologist",
+            "Physiotherapist",
+            "Occupational Therapist",
+            "Tele Health Psychologist"});
+            this.profession1.Location = new System.Drawing.Point(245, 156);
+            this.profession1.Name = "profession1";
+            this.profession1.Size = new System.Drawing.Size(253, 28);
+            this.profession1.TabIndex = 33;
+            this.profession1.Text = "GENERAL PRACTITIONER";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Profession:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1822, 1238);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sample);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.XX2);
+            this.Controls.Add(this.XX1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.bill);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.metadesc);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.region);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.suburb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.profession1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox sample;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox XX2;
+        private System.Windows.Forms.TextBox XX1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox bill;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox metadesc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox region;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox suburb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox profession1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JD Maker/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JD_Maker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string qualifydocAU = "HEHEHHEHEHE";
+            string newword = suburb.Text;
+            string newreg = region.Text;
+            string currentText = sample.Text;
+            string profession = profession1.Text;
+            string billtype = bill.Text;
+            string xxx1 = XX1.Text;
+            string xxx2 = XX2.Text;
+
+            string contact1 = "";
+            string qualification = "";
+            if (profession == "General Practitioner")
+            {
+                contact1 = "gprecruitment@medfuture.com.au\r\nCheny +61 420 468 717 and Sayeda +61 3 7065 4322";
+                qualification = qualifydocAU;
+
+            }
+            else if (profession == "Physiotherapist")
+            {
+                contact1 = "gprecruitment@medfuture.com.au\r\nCheny +61 420 468 717 and Sayeda +61 3 7065 4322";
+                qualification = "Physio Shit";
+
+            }
+            else
+            {
+                MessageBox.Show("ERROR" + profession);
+                return;
+            }
+
+
+
+
+
+
+            Dictionary<string, string> replacements = new Dictionary<string, string>
+    {
+        { "111", newword },
+        { "112", newreg },
+        { "Professionx",profession},
+        { "xbillings",billtype },
+        { "ContactDETAILS",contact1 },
+        {"X1",xxx1 },
+        {"X2",xxx2 },
+        {"QUALIFICATION DATA",qualification },
+            };
+
+            foreach (var replacement in replacements)
+            {
+                currentText = currentText.Replace(replacement.Key, replacement.Value);
+            }
+
+            sample.Text = currentText;
+        }
+    }
+}
